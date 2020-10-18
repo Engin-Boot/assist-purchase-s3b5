623de846ee0614c613d04167769256424c01e777
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-19 00:53:33 +0530

    Add files via upload

---
+++ src/main/java/com/philips/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.philips;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AssistPurchases3b5Application.class);
+	}
+
+}
