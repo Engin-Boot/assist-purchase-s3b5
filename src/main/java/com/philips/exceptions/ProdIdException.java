0238505d0bbff999dd11ff2234879d3e4087199a
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-19 00:56:14 +0530

    Add files via upload

---
+++ src/main/java/com/philips/exceptions/ProdIdException.java
@@ -0,0 +1,18 @@
+package com.philips.exceptions;
+
+@SuppressWarnings("serial")
+public class ProdIdException extends Exception {
+
+	public ProdIdException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProdIdException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	}
+	
