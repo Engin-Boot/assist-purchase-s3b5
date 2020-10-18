effade0cb7604a5edbe120878c9a96a566e3ea00
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-19 01:08:22 +0530

    Add files via upload

---
+++ src/main/resources/import.sql
@@ -0,0 +1,76 @@
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 6388, "IntelliVue MX40", 17999, "2020-05-13", true, true, false, true, false, true, true ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 9473, "IntelliVue MX750", 24599, "2020-04-06", false, true, true, false, true, false, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 3386, "IntelliVue MP2", 12399, "2019-12-20", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+insert into philips_products( prod_id, prod_name, prod_price, prod_dom, is_touchscreen, 
+        has_alarm, is_interoperable, is_wearable, has_emr_connectivity, is_portable, is_wireless )
+        values( 1762, "IntelliVue X3", 12399, "2020-02-25", false, false, true, false, false, true, false ) ;
+    
