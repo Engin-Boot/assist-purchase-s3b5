0f6fa67dd67761d77a06482b3855584afcb27e95
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-19 11:04:36 +0530

    Update AssistPurchaseApplication.java

--- src/main/java/com/philips/AssistPurchaseApplication.java
+++ src/main/java/com/philips/AssistPurchaseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssistPurchases3b5Application {
+public class AssistPurchaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssistPurchases3b5Application.class, args);
+		SpringApplication.run(AssistPurchaseApplication.class, args);
 		
 	}
 
