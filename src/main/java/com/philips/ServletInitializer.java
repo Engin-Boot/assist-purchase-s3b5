cf2178ddad9e36ef38af83fc8fed6c53f48abb04
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-19 11:23:56 +0530

    Update ServletInitializer.java

--- src/main/java/com/philips/ServletInitializer.java
+++ src/main/java/com/philips/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AssistPurchases3b5Application.class);
+		return application.sources(AssistPurchaseApplication.class);
 	}
 
 }
